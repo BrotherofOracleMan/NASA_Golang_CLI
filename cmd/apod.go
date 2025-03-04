f49a913571d135e22eb6cab831a6f5181e67aa36
Jiunn Siow <gene@Jiunns-MBP.attlocal.net>
2025-03-04 09:30:54 -0800

    working on apod now but there are still bugs

--- cmd/apod.go
+++ cmd/apod.go
@@ -1,1 +1,56 @@
 package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/BrotherofOracleMan/NASA_GOLANG_CLI/config"
+	cobra "github.com/spf13/cobra"
+)
+
+//only flag that we need to provide is date
+
+type ApiResponse struct {
+	Copyright       string `json:"copyright"`
+	Date            string `json:"date"`
+	Explanation     string `json:"explanation"`
+	Hdurl           string `json:"hdurl"`
+	Media_type      string `json:"media_type"`
+	Service_version string `json:"service_version"`
+	Title           string `json:"title"`
+	Url             string `json:"url"`
+}
+
+var datetime string
+
+var apod_cmd = &cobra.Command{
+	Use:   "apod",
+	Short: "Get the Astronomy Picture of the Day from NASA's astronomy picture of the day API",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		resp, err := http.Get(config.GetAPODURL() + "?api_key=" + config.GetAPIKey())
+
+		if err != nil {
+			fmt.Println("Unable to get HTTP response", err)
+		}
+		defer resp.Body.Close()
+
+		var apiResp ApiResponse
+		err = json.NewDecoder(resp.Body).Decode(&apiResp)
+
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+		}
+
+		fmt.Printf("Explanation : %s\n", apiResp.Explanation)
+		fmt.Printf("Title : %s\n", apiResp.Title)
+		fmt.Printf("URL : %s\n", apiResp.Url)
+		fmt.Printf("HD URL : %s\n", apiResp.Hdurl)
+	},
+}
+
+func init() {
+	apod_cmd.Flags().StringVarP(&datetime, "date", "d", "", "Date of the picture you want to see")
+	rootCmd.AddCommand(apod_cmd)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -8,19 +8,11 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "NASA CLI application ",
+	Use:   "rootcmd",
 	Short: "NASA CLI application for getting pictures of the Stars and planets",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Call the function to get the data from the API{
 		config.Loadconfig()
-
-		apod_url := config.GetAPODURL()
-		earth_date_url := config.GetEarthDateURL()
-		mars_rover_url := config.GetMarsRoverURL()
-
-		fmt.Printf("APOD URL: %s\n", apod_url)
-		fmt.Printf("Earth Date URL: %s\n", earth_date_url)
-		fmt.Printf("Mars Rover URL: %s\n", mars_rover_url)
 	},
 }
 
