94aa277cecd1a7ae5ad09d4d25c54ea33ab8fdec
Jiunn Siow <gene@Jiunns-MBP.attlocal.net>
2025-03-04 23:31:07 -0800

    tested golang's datetime libraries

--- cmd/apod.go
+++ cmd/apod.go
@@ -2,14 +2,18 @@ package cmd
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/BrotherofOracleMan/NASA_GOLANG_CLI/config"
 	cobra "github.com/spf13/cobra"
 )
 
 //only flag that we need to provide is date
+
+const format = "2006-01-02"
 
 type ApiResponse struct {
 	Copyright       string `json:"copyright"`
@@ -23,12 +27,12 @@ type ApiResponse struct {
 }
 
 var datetime string
+var download bool
 
 var apod_cmd = &cobra.Command{
 	Use:   "apod",
 	Short: "Get the Astronomy Picture of the Day from NASA's astronomy picture of the day API",
 	Run: func(cmd *cobra.Command, args []string) {
-
 		resp, err := http.Get(config.GetAPODURL() + "?api_key=" + config.GetAPIKey())
 
 		if err != nil {
@@ -47,10 +51,55 @@ var apod_cmd = &cobra.Command{
 		fmt.Printf("Title : %s\n", apiResp.Title)
 		fmt.Printf("URL : %s\n", apiResp.Url)
 		fmt.Printf("HD URL : %s\n", apiResp.Hdurl)
+
+		//check if the date flag is set
+		flag.Parse()
+
+		if datetime != "" {
+			//YYYY-MM-DD is the required format
+			test_parse, err := time.Parse(format, datetime)
+			formatted_date := test_parse.Format(format)
+			if err != nil {
+				fmt.Println("An Error occured when parsing the date in to YYYY-MM-DD", err)
+			}
+			fmt.Printf("Date : %s\n", test_parse)
+			fmt.Printf("Formatted Date : %s\n", formatted_date)
+			fmt.Println(test_parse)
+		}
+		//check if the download flag is set
+		if download {
+			fmt.Println("Downloading the image")
+		}
 	},
+}
+
+func build_api_request(date string, download bool) string {
+	return config.GetAPODURL() + "?api_key=" + config.GetAPIKey()
+	/*
+		resp, err := http.Get(config.GetAPODURL() + "?api_key=" + config.GetAPIKey())
+
+		if err != nil {
+			fmt.Println("Unable to get HTTP response", err)
+		}
+		defer resp.Body.Close()
+
+		var apiResp ApiResponse
+		err = json.NewDecoder(resp.Body).Decode(&apiResp)
+
+		if err != nil {
+			fmt.Println("Error decoding JSON:", err)
+		}
+
+		fmt.Printf("Explanation : %s\n", apiResp.Explanation)
+		fmt.Printf("Title : %s\n", apiResp.Title)
+		fmt.Printf("URL : %s\n", apiResp.Url)
+		fmt.Printf("HD URL : %s\n", apiResp.Hdurl)
+	*/
 }
 
 func init() {
 	apod_cmd.Flags().StringVarP(&datetime, "date", "d", "", "Date of the picture you want to see")
+	apod_cmd.Flags().BoolVarP(&download, "download", "D", false, "Download the picture of the day")
 	rootCmd.AddCommand(apod_cmd)
+	config.Loadconfig()
 }
