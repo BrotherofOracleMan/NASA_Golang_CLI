080199f1b89757ec9278b9dc6f42255d49a7b41b
BrotherofOracleMan <jsiow002.dev@gmail.com>
2025-03-06 23:59:45 -0800

    finished initial apod command

--- cmd/apod.go
+++ cmd/apod.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/BrotherofOracleMan/NASA_GOLANG_CLI/config"
@@ -33,68 +34,62 @@ var apod_cmd = &cobra.Command{
 	Use:   "apod",
 	Short: "Get the Astronomy Picture of the Day from NASA's astronomy picture of the day API",
 	Run: func(cmd *cobra.Command, args []string) {
-		resp, err := http.Get(config.GetAPODURL() + "?api_key=" + config.GetAPIKey())
+		flag.Parse()
+
+		var apiResp ApiResponse
+		apiURL, err := build_api_request(datetime)
+		fmt.Println(apiURL)
 
 		if err != nil {
-			fmt.Println("Unable to get HTTP response", err)
+			fmt.Println("Failed to build Api request. See error %w", err)
+			return
+		}
+		resp, err := http.Get(apiURL)
+
+		if err != nil {
+			fmt.Println("Failed to Get API response. See ErrorL %w", err)
+			return
 		}
 		defer resp.Body.Close()
 
-		var apiResp ApiResponse
 		err = json.NewDecoder(resp.Body).Decode(&apiResp)
 
 		if err != nil {
-			fmt.Println("Error decoding JSON:", err)
+			fmt.Println("Failed to Decode API response. See Error %w", err)
+			return
 		}
 
 		fmt.Printf("Explanation : %s\n", apiResp.Explanation)
 		fmt.Printf("Title : %s\n", apiResp.Title)
 		fmt.Printf("URL : %s\n", apiResp.Url)
 		fmt.Printf("HD URL : %s\n", apiResp.Hdurl)
-
-		//check if the date flag is set
-		flag.Parse()
-
-		if datetime != "" {
-			//YYYY-MM-DD is the required format
-			test_parse, err := time.Parse(format, datetime)
-			formatted_date := test_parse.Format(format)
-			if err != nil {
-				fmt.Println("An Error occured when parsing the date in to YYYY-MM-DD", err)
-			}
-			fmt.Printf("Date : %s\n", test_parse)
-			fmt.Printf("Formatted Date : %s\n", formatted_date)
-			fmt.Println(test_parse)
-		}
-		//check if the download flag is set
-		if download {
-			fmt.Println("Downloading the image")
-		}
 	},
 }
 
-func build_api_request(date string, download bool) string {
-	return config.GetAPODURL() + "?api_key=" + config.GetAPIKey()
-	/*
-		resp, err := http.Get(config.GetAPODURL() + "?api_key=" + config.GetAPIKey())
+func build_api_request(date string) (string, error) {
+	baseUrl := config.GetAPODURL()
 
+	parsedURL, err := url.Parse(baseUrl)
+
+	if err != nil {
+		return "", fmt.Errorf("an error occured while parsing the URL %w", err)
+	}
+
+	queryParams := parsedURL.Query()
+
+	if date != "" {
+		//YYYY-MM-DD is the required format
+		parsed_date, err := time.Parse(format, datetime)
 		if err != nil {
-			fmt.Println("Unable to get HTTP response", err)
+			return "", fmt.Errorf("an Error occured when parsing the date in to YYYY-MM-DD: %w", err)
 		}
-		defer resp.Body.Close()
+		formatted_date := parsed_date.Format(format)
+		queryParams.Set("date", formatted_date)
+	}
+	queryParams.Set("api_key", config.GetAPIKey())
+	parsedURL.RawQuery = queryParams.Encode()
+	return parsedURL.String(), nil
 
-		var apiResp ApiResponse
-		err = json.NewDecoder(resp.Body).Decode(&apiResp)
-
-		if err != nil {
-			fmt.Println("Error decoding JSON:", err)
-		}
-
-		fmt.Printf("Explanation : %s\n", apiResp.Explanation)
-		fmt.Printf("Title : %s\n", apiResp.Title)
-		fmt.Printf("URL : %s\n", apiResp.Url)
-		fmt.Printf("HD URL : %s\n", apiResp.Hdurl)
-	*/
 }
 
 func init() {
